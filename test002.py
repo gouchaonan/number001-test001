a33dda4068776573e6778ad022249dcbe2a922d9
gouchaonan <1432519606@qq.com>
2023-03-02 18:44:44 +0800

    Changes

---
+++ test002.py
@@ -0,0 +1,22 @@
+# L = [[1, 2, 3], [5, 3, 2], [7, 3, 2]]
+# for cube in L:
+#     length = cube[0]
+#     print(length)
+#
+#     width = cube[1]
+#     print(width)
+#
+#     heigtht = cube[2]
+#     print(heigtht)
+#
+#     re = length * width * 2 + width * heigtht * 2 + length * heigtht * 2
+#
+#     print(re)
+#     print("hello ")
+
+
+#
+# numbersInList = [x for x in range(10)]
+# print(numbersInList)
+# numbersInTuple = tuple(numbersInList)
+# print(numbersInTuple)
